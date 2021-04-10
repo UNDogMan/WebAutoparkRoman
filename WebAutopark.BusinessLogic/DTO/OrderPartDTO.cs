57410e27c69c37fff1b5187baf897b9ca66f566e
Zlobin Roman <roma.zlobin74@gmail.com>
2021-04-10 12:00:09 +0300

    Complite DTO

---
+++ WebAutopark.BusinessLogic/DTO/OrderPartDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAutopark.BusinessLogic.DTO
+{
+    public class OrderPartDTO
+    {
+        public int ID { get; set; }
+        public int PartID { get; set; }
+        public int OrderID { get; set; }
+        public int PartCount { get; set; }
+    }
+}
---
+++ WebAutopark.BusinessLogic/DTO/OrederDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAutopark.BusinessLogic.DTO
+{
+    public class OrederDTO
+    {
+        public int ID { get; set; }
+        public int VehicleID { get; set; }
+    }
+}
