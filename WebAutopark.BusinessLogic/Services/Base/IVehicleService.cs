c9eeaebf448979ff56886b7f9378edcf01e0b34b
Zlobin Roman <roma.zlobin74@gmail.com>
2021-04-13 20:01:51 +0300

    Write Interface for Vehicle Service

---
+++ WebAutopark.BusinessLogic/Services/Base/IVehicleService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAutopark.BusinessLogic.DTO;
+
+namespace WebAutopark.BusinessLogic.Services.Base
+{
+    public interface IVehicleService : IBaseService<VehicleDTO>
+    {
+        float GetTaxPerMount(VehicleDTO vehicle);
+        float GetMaxmileage(VehicleDTO vehicle);
+    }
+}
