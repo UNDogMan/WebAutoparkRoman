75179ca768be17c447c18c9980e7cc7cb07349a0
Zlobin Roman <roma.zlobin74@gmail.com>
2021-04-14 20:04:48 +0300

    Add Extensions For DI

---
+++ WebAutopark.BusinessLogic/Extensions/AutoMapperExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using AutoMapper.Execution;
+using WebAutopark.DataAccess.Entities;
+using WebAutopark.BusinessLogic.DTO;
+
+namespace WebAutopark.BusinessLogic.Extensions
+{
+    public static class AutoMapperExtension
+    {
+        public static void AddMapper(this IServiceCollection services)
+        {
+            services.AddAutoMapper(cfg =>
+            {
+                cfg.CreateMap<Order, OrderDTO>().ReverseMap();
+                cfg.CreateMap<Part, PartDTO>().ReverseMap();
+                cfg.CreateMap<OrderPart, OrderPartDTO>().ReverseMap();
+                cfg.CreateMap<VehicleType, VehicleTypeDTO>().ReverseMap();
+                cfg.CreateMap<Vehicle, VehicleDTO>().ReverseMap();
+            });
+        }
+    }
+}
---
+++ WebAutopark.BusinessLogic/Extensions/BusinessLogicExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAutopark.BusinessLogic.DTO;
+using WebAutopark.BusinessLogic.Services;
+using WebAutopark.BusinessLogic.Services.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebAutopark.BusinessLogic.Extensions
+{
+    public static class BusinessLogicExtension
+    {
+        public static void AddBusinessLogin(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseService<OrderDTO>, OrderService>();
+            services.AddScoped<IBaseService<PartDTO>, PartService>();
+            services.AddScoped<IBaseService<OrderPartDTO>, OrderPartService>();
+            services.AddScoped<IBaseService<VehicleTypeDTO>, VehicleTypeServise>();
+            services.AddScoped<IVehicleService, VehicleService>();
+        }
+    }
+}
