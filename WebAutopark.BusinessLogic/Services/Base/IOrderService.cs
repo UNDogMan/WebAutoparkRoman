674ad45b81f712585ec7f0f3648e033aad54f985
Zlobin Roman <roma.zlobin74@gmail.com>
2021-04-19 18:02:09 +0300

    Add BLL Changes

---
+++ WebAutopark.BusinessLogic/Services/Base/IOrderService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAutopark.BusinessLogic.Dto;
+
+namespace WebAutopark.BusinessLogic.Services.Base
+{
+    public interface IOrderService : IBaseService<OrderDto>
+    {
+        Task ClearOrder(int id);
+        Task<int> CreateWithID(OrderDto item);
+    }
+}
